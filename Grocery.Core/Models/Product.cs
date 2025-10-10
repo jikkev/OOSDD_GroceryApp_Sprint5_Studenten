98366af9f8fa7bdb7f84dec7079bf3c20f09d891
jikkev <s1198343@student.windesheim.nl>
2025-10-10 12:43:35 +0200

    errors wegwerken

--- Grocery.Core/Models/Product.cs
+++ Grocery.Core/Models/Product.cs
@@ -1,12 +1,11 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
-using System.Diagnostics;
+using System;
 
 namespace Grocery.Core.Models
 {
     public partial class Product : Model
     {
         [ObservableProperty]
-
         private int stock;
 
         [ObservableProperty]
@@ -19,21 +18,11 @@ namespace Grocery.Core.Models
 
         public Product(int id, string name, int stock, DateOnly shelfLife, decimal price)
             : base(id, name)
-
-        public int stock;
-        public decimal Price { get; set; }
-        public DateOnly ShelfLife { get; set; }
-        public Product(int id, string name, int stock) : this(id, name, stock, default, default) { }
-
-        public Product(int id, string name, int stock, DateOnly shelfLife) : this(id, name, stock, shelfLife, 0.0m) { }
-        public Product(int id, string name, int stock, DateOnly shelfLife, decimal price): base(id, name)
-
         {
             Stock = stock;
             ShelfLife = shelfLife;
             Price = price;
         }
-
 
         public Product(int id, string name, int stock)
             : this(id, name, stock, default, default) { }
@@ -42,9 +31,6 @@ namespace Grocery.Core.Models
             : this(id, name, stock, shelfLife, default) { }
 
         public override string ToString()
-
-        public override string? ToString()
-
         {
             return $"{Name} - {Stock} op voorraad - € {Price}";
         }
